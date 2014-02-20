9a2f5a7c24fd79602ac80aae49317697924e4521
Anesh Ramchurran <aneshramchurran@bossa.home>
2014-02-20 16:25:50 -0700

    Initial Commit

---
+++ ss/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ss
+//
+//  Created by Anesh Ramchurran on 2/20/2014.
+//  Copyright (c) 2014 Anesh Ramchurran. All rights reserved.
+//
+
+#include <CoreServices/CoreServices.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
